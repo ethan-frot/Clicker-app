bebf76ece2004123fe47494313b80f4042699b36
Ethan Frot <contact@ethanfrot.com>
2025-03-26 08:59:25 +0100

    🥅Remove api keys to use .env

--- app/utils/database.ts
+++ app/utils/database.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKkaKUK28WXwnxdl6i6-Gwa3IZDeAjshE",
-  authDomain: "clicker-66580.firebaseapp.com",
-  projectId: "clicker-66580",
-  storageBucket: "clicker-66580.firebasestorage.app",
-  messagingSenderId: "77556637399",
-  appId: "1:77556637399:web:dad902f6f4b4378a239a92"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
